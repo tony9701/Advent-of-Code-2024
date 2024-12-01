c650aa7a638764ca20de8740761991db4e46235a
tony9701 <leesin9701@gmail.com>
2024-12-01 19:57:47 +0200

    second problem answer

---
+++ src/SecondProblem.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SecondProblem {
+    public static void main(String[] args) {
+
+
+        List<Integer> leftList = new ArrayList<Integer>();
+        List<Integer> rightList = new ArrayList<Integer>();
+
+        ListCreator.createLists(leftList, rightList);
+
+        int answer = 0;
+
+        for (Integer left : leftList) {
+            int repeatCounter = 0;
+
+            for (Integer right : rightList) {
+                if ((int)left == right) {
+                    repeatCounter++;
+                }
+            }
+
+            answer += left * repeatCounter;
+        }
+
+        System.out.println(answer);
+    }
+}

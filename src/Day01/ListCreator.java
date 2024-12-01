d7d4fd18e5966795257337e7aec00ba050f7e6e0
tony9701 <leesin9701@gmail.com>
2024-12-01 19:59:26 +0200

    Package refactoring

--- src/Day01/ListCreator.java
+++ src/Day01/ListCreator.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+package Day01;
+
 import java.util.List;
 import java.util.Scanner;
 
--- src/Day01/SecondProblem.java
+++ src/Day01/SecondProblem.java
@@ -1,6 +1,7 @@
+package Day01;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class SecondProblem {
     public static void main(String[] args) {

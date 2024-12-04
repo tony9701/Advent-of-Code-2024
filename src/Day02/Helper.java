e075a0fa8627ac6750a3ad0398eae821949bb844
tony9701 <leesin9701@gmail.com>
2024-12-04 19:47:38 +0200

    deleted the not needed code!

--- src/Day02/Helper.java
+++ src/Day02/Helper.java
@@ -1,6 +1,5 @@
 package Day02;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -10,25 +9,15 @@ public class Helper {
 
     public static boolean isSafe(List<Integer> numbers) {
 
-        if (isDecreasingV2(numbers)) {
+        if (isDecreasing(numbers)) {
             return true;
         }
 
-        return isIncreasingV2(numbers);
+        return isIncreasing(numbers);
 
     }
 
-    public static boolean isSafe(List<Integer> numbers, boolean isProblemTwo) {
-
-        if (isDecreasing(numbers, true)) {
-            return true;
-        }
-
-        return isIncreasing(numbers, true);
-
-    }
-
-    public static boolean isIncreasingV2(List<Integer> numbers) {
+    public static boolean isIncreasing(List<Integer> numbers) {
         for (int i = 0; i < numbers.size() - 1; i++) {
             int diff = numbers.get(i) - numbers.get(i + 1);
 
@@ -41,7 +30,7 @@ public class Helper {
     }
 
 
-    public static boolean isDecreasingV2(List<Integer> numbers) {
+    public static boolean isDecreasing(List<Integer> numbers) {
         for (int i = 0; i < numbers.size() - 1; i++) {
             int diff = numbers.get(i + 1) - numbers.get(i);
 
@@ -51,133 +40,6 @@ public class Helper {
         }
 
         return true;
-    }
-
-
-    public static boolean isIncreasing(List<Integer> numbers) {
-        for (int i = 0; i < numbers.size() - 1; i++) {
-            int currentNum = numbers.get(i);
-            int nextNum = numbers.get(i + 1);
-
-            //check if the difference between numbers are more than 3 or if the nums are equal
-            if (diffCheckerNotValid(currentNum, nextNum)) {
-                return false;
-            }
-
-            //check if the number is increasing (the next num is bigger)
-            if (currentNum > nextNum) {
-                return false;
-            }
-
-        }
-
-        return true;
-    }
-
-    public static boolean isIncreasing(List<Integer> numbers, boolean isRemovingLevel) {
-        int removedLevelsFlag = 0;
-        List<Integer> numberCopy = new ArrayList<>(numbers);
-
-        for (int i = 0; i < numberCopy.size() - 1; i++) {
-            int currentNum = numberCopy.get(i);
-            int nextNum = numberCopy.get(i + 1);
-
-            //check if the difference between numbers are more than 3 or if the nums are equal
-            if (diffCheckerNotValid(currentNum, nextNum)) {
-
-                if (removedLevelsFlag == 0) {
-                    numberCopy.remove(i + 1);
-                    removedLevelsFlag++;
-                    i--;
-                    continue;
-                }
-
-                return false;
-            }
-
-
-            //check if the number is increasing (the next num is bigger)
-            if (currentNum > nextNum) {
-
-                //check if level is already removed. If not remove it and restart the loop.
-                if (removedLevelsFlag == 0) {
-                    numberCopy.remove(i + 1);
-                    removedLevelsFlag++;
-                    i--;
-                    continue;
-                }
-
-                return false;
-            }
-
-
-        }
-
-        return true;
-    }
-
-
-
-    public static boolean isDecreasing(List<Integer> numbers) {
-        for (int i = 0; i < numbers.size() - 1; i++) {
-            int currentNum = numbers.get(i);
-            int nextNum = numbers.get(i + 1);
-
-            //check if the difference between numbers are more than 3 or if the nums are equal
-            if (diffCheckerNotValid(currentNum, nextNum)) {
-                return false;
-            }
-
-            //check if the number is decreasing (the next num is lower)
-            if (currentNum < nextNum) {
-                return false;
-            }
-
-        }
-
-        return true;
-    }
-
-    public static boolean isDecreasing(List<Integer> numbers, boolean isRemovingLevel) {
-        int removedLevelsFlag = 0;
-        List<Integer> numberCopy = new ArrayList<>(numbers);
-
-        for (int i = 0; i < numberCopy.size() - 1; i++) {
-            int currentNum = numberCopy.get(i);
-            int nextNum = numberCopy.get(i + 1);
-
-            //check if the difference between numbers are more than 3 or if the nums are equal
-            if (diffCheckerNotValid(currentNum, nextNum)) {
-                if (removedLevelsFlag == 0) {
-                    numberCopy.remove(i + 1);
-                    removedLevelsFlag++;
-                    i--;
-                    continue;
-                }
-
-                return false;
-            }
-
-            //check if the number is decreasing (the next num is lower)
-            if (currentNum < nextNum) {
-                if (removedLevelsFlag == 0) {
-                    numberCopy.remove(i + 1);
-                    removedLevelsFlag++;
-                    i--;
-                    continue;
-                }
-
-                return false;
-            }
-
-
-        }
-
-        return true;
-    }
-
-    public static boolean diffCheckerNotValid(int currentNum, int nextNum) {
-        return Math.abs(currentNum - nextNum) > 3 || nextNum == currentNum;
     }
 
     public static void listCreator(List<List<Integer>> numbersList) {

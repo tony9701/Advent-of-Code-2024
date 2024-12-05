1decb4a94f6219b0a72c4b55034c39bed17af54f
tony9701 <leesin9701@gmail.com>
2024-12-05 18:40:49 +0200

    data checks

--- src/Day03/FirstProblem.java
+++ src/Day03/FirstProblem.java
@@ -24,19 +24,30 @@ public class FirstProblem {
             input.append(line);
             input.append(System.lineSeparator());
         }
-        
+
         Matcher matcher = pattern.matcher(input);
 
 
         Map<Integer, Integer> matches = new HashMap<>();
+        List<Integer> listTest = new ArrayList<>();
 
         while (matcher.find()) {
             matches.put(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)));
+            listTest.add(Integer.parseInt(matcher.group(1)) * Integer.parseInt(matcher.group(2)));
+        }
+
+        long listCount = listTest.stream().mapToLong(l -> l).sum();
+        long listCount2 = 0;
+
+        for (Integer i : listTest) {
+            listCount2 += i;
         }
 
         long count = matches.entrySet().stream().mapToLong(e -> (long) e.getKey() * e.getValue()).sum();
 
         System.out.println("\n" + count);
+        System.out.println("\n" + listCount);
+        System.out.println("\n" + listCount2);
 
     }
 }
